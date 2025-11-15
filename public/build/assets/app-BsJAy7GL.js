4de95849e36779a059c7857a8a7b9eb683dfd707
puskarwagl <puskarwagle17@gmail.com>
2025-11-15 19:41:34 +0545

    updates

---
+++ public/build/assets/app-BsJAy7GL.js
@@ -0,0 +1,1 @@
+import{a as o}from"./index-B9ygI19o.js";window.axios=o;window.axios.defaults.headers.common["X-Requested-With"]="XMLHttpRequest";
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'packages/Webkul/Admin/src/Resources/assets/css/app.css',
+                'packages/Webkul/Admin/src/Resources/assets/js/app.js',
+            ],
             refresh: true,
         }),
+        vue(),
     ],
+    build: {
+        rollupOptions: {
+            external: ['vue'],
+        },
+    },
 });
