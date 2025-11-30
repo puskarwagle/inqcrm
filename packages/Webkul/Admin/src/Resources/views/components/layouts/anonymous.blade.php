752407da7bfdfb3d09550cd410849b68fd4f1da1
puskarwagl <puskarwagle17@gmail.com>
2025-11-30 11:04:17 +0545

    build assets

--- packages/Webkul/Admin/src/Resources/views/components/layouts/anonymous.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/layouts/anonymous.blade.php
@@ -34,12 +34,10 @@
 
     @stack('meta')
 
-    {{
-        vite()->set([
-            'packages/Webkul/Admin/src/Resources/assets/css/app.css',
-            'packages/Webkul/Admin/src/Resources/assets/js/app.js'
-        ])
-    }}
+    @vite([
+        'packages/Webkul/Admin/src/Resources/assets/css/app.css',
+        'packages/Webkul/Admin/src/Resources/assets/js/app.js'
+    ])
 
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap"
